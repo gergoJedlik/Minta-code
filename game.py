5216d0b6005a561d45bc0f8ed451744ab2a7b06e
Gergo <kapusigele@gmail.com>
2024-02-12 14:08:04 +0100

    LEADERBOARD (work in prog)

--- game.py
+++ game.py
@@ -1,5 +1,7 @@
 import pygame
 import random
+import sqlite3
+    
 
 #kiírja a pontszámot jaték közben
 def display_score():
@@ -9,6 +11,7 @@ def display_score():
 
 #kiírja a pontszámot és időt jaték után
 def display_final_score():
+    cur.execute(f"INSERT INTO leaderboard ({name}) VALUES ({score})")
     final_score_surf = game_font.render("PONTSZÁM: " + str(score), True, BLUE)
     final_score_rect = final_score_surf.get_rect(center=(WIDTH / 2, HEIGHT - 220))
     screen.blit(final_score_surf, final_score_rect)
@@ -28,6 +31,28 @@ def display_lives_left():
     )
     lives_left_rect = lives_left_surf.get_rect(topleft=(10, 10))
     screen.blit(lives_left_surf, lives_left_rect)
+
+def display_leaderboard(leader_db: dict):
+    leader_surf = game_font.render(
+        "Ranglista: ", True, BLUE
+    )
+    leader_rect = leader_surf.get_rect(topright = (WIDTH-10, 10))
+    screen.blit(leader_surf, leader_rect)
+    i = 1
+    for name, score in leader_db.items():
+        leader_surf = game_font.render(
+            str(i) + ".   " + str(name) + "    " + str(score), True, BLUE
+        )
+
+        leader_rect = leader_surf.get_rect(topright = (WIDTH-10, 50*i))
+
+        screen.blit(leader_surf, leader_rect)
+        i+=1
+
+
+
+        
+
 
 #legenerálja a hátteret
 def BG_Generation():
@@ -85,6 +110,20 @@ title_rect = title_surf.get_rect(center=(WIDTH / 2, 200))
 run_surf = game_font.render("Kezdéshez nyomd meg a szóközt!", True, BLUE)
 run_rect = run_surf.get_rect(center=(WIDTH / 2, HEIGHT - 150))
 
+con = sqlite3.connect("Leaderboard.db")
+cur = con.cursor()
+try:
+    cur.execute("CREATE TABLE leaderboard(name, score)")
+except:
+    pass
+
+user_text = '' 
+input_rect = pygame.Rect(WIDTH//2 - 45, HEIGHT-90, 400, 50)  
+color_active = pygame.Color('lightskyblue3') 
+color_passive = pygame.Color('chartreuse4') 
+color = color_passive
+active = False
+
 start_time = pygame.time.get_ticks()
 score = 0
 lives = 5
@@ -100,6 +139,20 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if not game_active:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if input_rect.collidepoint(event.pos):
+                    active = True
+                else: 
+                    active = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                game_active = False
+            if not game_active:
+                if event.key == pygame.K_BACKSPACE:
+                    user_text = user_text[:-1]
+                else: 
+                    user_text += event.unicode
         #Curor lekövetése
         if event.type == pygame.MOUSEMOTION:
             sack_rect = sack_surf.get_rect(center=event.pos)
@@ -112,8 +165,21 @@ while running:
         if event.type == speedup_timer:
             star_speed += 2
             new_spawnrate -= 50
+    
+    if active:
+        input_color = color_active
+    else:
+        input_color = color_passive
+
+    pygame.draw.rect(screen, color, input_rect)
+    text_surface = game_font.render(user_text, True, (255, 255, 255))
+    screen.blit(text_surface, (input_rect.x+5, input_rect.y+5)) 
+    input_rect.w = max(100, text_surface.get_width()+10) 
+    pygame.display.flip()
 
     screen.blit(bg_surf, bg_rect)
+
+    
 
     if game_active:
         #Életek számlálása
@@ -156,6 +222,9 @@ while running:
         screen.blit(title_surf, title_rect)
         screen.blit(star_surf, star_surf.get_rect(center=(WIDTH / 2, HEIGHT / 2)))
         screen.blit(run_surf, run_rect)
+        bamm: dict[str, int] = {"gege": 45, "test": 33, "testver": 12, "csíí": 99}
+        display_leaderboard(bamm)
+        # cur.execute("SELECT name, score FROM leaderboard ORDER BY score DESC")
 
         if score:
             display_final_score()
